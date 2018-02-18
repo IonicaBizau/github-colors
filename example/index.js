d84c8f3004f392d3336fcb934557d66443a6e92a
Ionică Bizău <bizauionica@gmail.com>
2018-02-18 09:44:15 +0200

    Add default color, fix #26

--- example/index.js
+++ example/index.js
@@ -1,4 +1,65 @@
 var GitHubColors = require("../lib");
 
 console.log(GitHubColors.get("C++"));
+// { type: 'programming',
+//   ace_mode: 'c_cpp',
+//   codemirror_mode: 'clike',
+//   codemirror_mime_type: 'text/x-c++src',
+//   color: '#f34b7d',
+//   aliases: [ 'cpp' ],
+//   extensions:
+//    [ '.cpp',
+//      '.c++',
+//      '.cc',
+//      '.cp',
+//      '.cxx',
+//      '.h',
+//      '.h++',
+//      '.hh',
+//      '.hpp',
+//      '.hxx',
+//      '.inc',
+//      '.inl',
+//      '.ipp',
+//      '.re',
+//      '.tcc',
+//      '.tpp' ],
+//   language_id: 43 }
+
 console.log(GitHubColors.ext("cpp"));
+// { extensions:
+//    [ '.cpp',
+//      '.c++',
+//      '.cc',
+//      '.cp',
+//      '.cxx',
+//      '.h',
+//      '.h++',
+//      '.hh',
+//      '.hpp',
+//      '.hxx',
+//      '.inc',
+//      '.inl',
+//      '.ipp',
+//      '.re',
+//      '.tcc',
+//      '.tpp' ],
+//   aliases: [ 'cpp' ],
+//   type: 'programming',
+//   ace_mode: 'c_cpp',
+//   codemirror_mode: 'clike',
+//   codemirror_mime_type: 'text/x-c++src',
+//   color: '#f34b7d',
+//   language_id: 43 }
+
+
+// CMake doesn't have a color, so we specify in the second argument that we need to have the default color.
+console.log(GitHubColors.get("CMake", true));
+// { type: 'programming',
+//   extensions: [ '.cmake', '.cmake.in' ],
+//   filenames: [ 'CMakeLists.txt' ],
+//   ace_mode: 'text',
+//   codemirror_mode: 'cmake',
+//   codemirror_mime_type: 'text/x-cmake',
+//   language_id: 47,
+//   color: '#ccc' }
